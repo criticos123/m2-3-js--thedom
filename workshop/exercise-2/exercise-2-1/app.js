d405ba619363eca2810bdaeff4a0de079db719c1
Stephanos Criticos <scritico@unb.ca>
2020-06-21 21:38:34 -0400

    workshop frog game

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -1,19 +1,21 @@
 // Preset values
-const main= document.querySelector('main');
+const main= document.querySelector('body');
 const FROGS = 3;
 const track= document.getElementById("track");
 let style= document.createElement('link');
 style.href= "../assets/styles.css";
 style.rel="stylesheet";
 main.appendChild(style);
+
 // 1. Create for loop that makes use of FROGS to know how many lanes to create.
-for(let i=1; i<=FROGS;i++){
+for(let i=0; i<FROGS;i++){
     const lane = document.createElement('li');
     track.appendChild(lane);
     const lanenumber = document.createElement('span');
-    lanenumber.innerText=i;
-    lane.id= "lane number:"+i;
+    lanenumber.innerText=i+1;
+    lane.id= "lanenumber"+i;
     lane.appendChild(lanenumber);
+    
 }
 
 // 2. Create li
@@ -27,23 +29,32 @@ for(let i=1; i<=FROGS;i++){
 let racers=[];
 
 for(let i=0;i<FROGS;i++){
-    const frogstable=[i];
-    racers.push(frogstable)
+    racers.push(frogStable[i]);
+    
 }
 console.log(racers)
 //Exercise 2-3
 
-racers.forEach(function (racer, id) {
-    const newlane= document.createElement('span')
-    newlane.classList.add('frog');
-    newlane.style.background= race.color;
-    document.getElementById(id).appendChild(newlane);
-    racers[id].progress=0;
-    racers[id].lane=id;
-    const froggo=document.createElement('span');
-    document.getElementById(id).appendChild(froggo);
-});
 
+racers.forEach(function (racer, i) {
+    console.log(racer);
+    let newFrog= document.createElement('span');
+    newFrog.innerText=racer.number;
+    newFrog.classList.add('frog');
+    console.log("lanenumber"+i);
+
+    const laneId= document.getElementById("lanenumber"+i);
+    laneId.appendChild(newFrog);
+});
+console.log(racers);
+setInterval(function(){
+    const frogs=document.getElementsByClassName('frog');
+    [...frogs].forEach(function(currentItem){
+        console.log(currentItem);
+        currentItem.style.left="10px";
+    });
+    
+},2000);
 
 
 
